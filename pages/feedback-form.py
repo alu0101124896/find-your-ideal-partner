2c50fa9a6158175b067115bf7fadc268d09a35ee
carlasanmiguel <carlasanmiguelf@gmail.com>
2025-07-01 20:30:10 +0200

    Update feedback-form.py

--- pages/feedback-form.py
+++ pages/feedback-form.py
@@ -10,11 +10,11 @@ import streamlit.components.v1 as components
 st.title("Formulario de opinón")
 
 google_form_embed_code = """
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfO3vW8yXCm57oNMjySB5NsGf6bLTG3an3J2OXtJppwMVWT8A/viewform?embedded=true" width="700" height="1000" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfO3vW8yXCm57oNMjySB5NsGf6bLTG3an3J2OXtJppwMVWT8A/viewform?embedded=true" width="700" height="600" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
 """
 
 
-components.html(google_form_embed_code, height=1000) # El 'height' aquí se refiere a la altura del componente en Streamlit
+components.html(google_form_embed_code, height=600) # El 'height' aquí se refiere a la altura del componente en Streamlit
 
 def main():
     # ToDo: Implement the feedback form page functionality
