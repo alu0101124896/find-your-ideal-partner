c960fe96ec213f7e9afba9d8f9e35cbcb0ed2e65
Sergio Tabares <alu0101124896@ull.edu.es>
2025-07-02 12:12:54 +0100

    Add new EDA graphs

--- pages/eda.py
+++ pages/eda.py
@@ -34,6 +34,7 @@ def main():
     """Main function to run the EDA page of the web application."""
 
     set_streamlit_page_config()
+    set_seaborn_style()
     init_session_state_variables()
 
     show_title_and_description()
@@ -46,6 +47,22 @@ def set_streamlit_page_config():
 
     st.set_page_config(
         layout="centered",
+    )
+
+
+def set_seaborn_style():
+    """Set the Seaborn style for visualizations."""
+
+    sns.set_theme(
+        context="paper",
+        style="white",
+        font_scale=1.2,
+        rc={
+            "axes.labelsize": 14,
+            "axes.titlesize": 16,
+            "xtick.labelsize": 12,
+            "ytick.labelsize": 12,
+        },
     )
 
 
@@ -131,6 +148,10 @@ def show_eda_visualizations():
     st.write("---")
     show_choropleth_map()
     st.write("---")
+    show_health_and_care_histograms()
+    st.write("---")
+    show_personality_and_behavior_histograms()
+    st.write("---")
     show_correlation_heatmap()
 
 
@@ -139,7 +160,7 @@ def show_age_size_gender_boxplot():
 
     st.subheader("Distribución de edades según el tamaño y el género")
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(8, 6))
     sns.boxplot(
         x="size",
         y="age",
@@ -148,9 +169,12 @@ def show_age_size_gender_boxplot():
         data=st.session_state.dogs_df.loc[(st.session_state.dogs_df["age"] < 50)],
         ax=ax,
     )
-    plt.legend(title="Género")
+
+    plt.legend(title="Género", loc="upper right")
     ax.set_xlabel("Tamaño")
     ax.set_ylabel("Edad")
+
+    plt.tight_layout()
     st.pyplot(fig)
 
 
@@ -159,7 +183,7 @@ def show_choropleth_map():
 
     st.subheader("Mapa de disponibilidad por provincia")
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 10))
 
     dogs_per_province = (
         st.session_state.dogs_df.groupby("province", observed=True)
@@ -185,8 +209,105 @@ def show_choropleth_map():
             "shrink": 0.8,
         },
     )
+
     plt.axis("off")
+
     st.pyplot(fig)
+
+
+def show_health_and_care_histograms():
+    """Display histograms of health and care features in the dataset"""
+
+    st.subheader("Distribución de características de salud y cuidado")
+
+    health_and_care_features = [
+        "is_healthy",
+        "is_vaccinated",
+        "is_dewormed",
+        "is_sterilized",
+        "is_identified",
+        "has_microchip",
+        "has_passport",
+    ]
+    health_and_care_stats = get_stats(health_and_care_features)
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # Create a bar plot to visualize the distribution of health and care features
+    sns.barplot(
+        ax=ax,
+        data=health_and_care_stats,
+        x="value",
+        y="count",
+        hue="variable",
+        palette="pastel",
+        order=["True", "False"],  # Ensure the order of the bars is consistent
+    )
+
+    ax.legend(title="Características", loc="upper right")
+    ax.set_xlabel("Características de salud y cuidado")
+    ax.set_ylabel("Número de perros")
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+
+def show_personality_and_behavior_histograms():
+    """Display histograms of personality and behavior features in the dataset"""
+
+    st.subheader("Distribución de características de personalidad y comportamiento")
+
+    personality_and_behavior_features = [
+        "is_affectionate",
+        "is_hyperactive",
+        "is_fearful",
+        "is_sociable",
+        "is_calm",
+        "is_sedentary",
+    ]
+    personality_and_behavior_stats = get_stats(personality_and_behavior_features)
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # Create a bar plot to visualize the distribution of health and care features
+    sns.barplot(
+        ax=ax,
+        data=personality_and_behavior_stats,
+        x="value",
+        y="count",
+        hue="variable",
+        palette="pastel",
+        order=["True", "False"],  # Ensure the order of the bars is consistent
+    )
+
+    ax.legend(title="Características", loc="upper left")
+    ax.set_xlabel("Características de personalidad y comportamiento")
+    ax.set_ylabel("Número de perros")
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+
+def get_stats(features):
+    """Get statistics for the specified features in the dataset."""
+
+    return (
+        st.session_state.dogs_df[features]
+        .melt(
+            value_vars=features,
+        )
+        .groupby(
+            ["value", "variable"],
+            group_keys=True,
+            observed=True,
+        )
+        .size()
+        .reset_index(name="count")
+        .sort_values(
+            by=["value", "variable"],
+            ascending=[False, False],
+        )
+    )
 
 
 def show_correlation_heatmap():
@@ -194,12 +315,14 @@ def show_correlation_heatmap():
 
     st.subheader("Mapa de correlación entre algunas de las características")
 
-    fig, ax = plt.subplots(figsize=(14, 14))
-    corr = st.session_state.visualization_df.corr()
+    features_correlations = st.session_state.visualization_df.corr()
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+
     sns.heatmap(
-        corr,
-        mask=np.triu(np.ones_like(corr, dtype=bool)),
+        features_correlations,
         ax=ax,
+        mask=np.triu(np.ones_like(features_correlations, dtype=bool)),
         annot=False,
         fmt=".2f",
         cmap="coolwarm",
@@ -209,6 +332,8 @@ def show_correlation_heatmap():
         vmax=1,
         linewidths=0.5,
     )
+
+    plt.tight_layout()
     st.pyplot(fig)
 
 
